972fa6171c7f65ed1d10aa556bd2f59f303bca1a
Kyle Phillips <kyle@haptic-data.com>
2014-06-29 17:18:38 -0700

    removed Stats.js, set all tabs to 4 spaces

--- template/javascripts/config.js
+++ template/javascripts/config.js
@@ -2,17 +2,15 @@
 requirejs.config({
     //points to vendor so that you don't have to
     //fill in `paths` config for every library
-	baseUrl: 'javascripts/vendor',
+    baseUrl: 'javascripts/vendor',
     //shim maps global objects to require.js modules,
     //meant for libraries not intended for use with require.js
-	shim: {
-		'async': { exports: 'async' },
-		'modernizr': { exports: 'Modernizr' },
-		'd3': { exports: 'd3' },
-		'three': { exports: 'THREE' },
-		'Stats': { exports: 'Stats' }
-	},
-	paths: {
-		'app': '../app'
-	}
+    shim: {
+        'modernizr': { exports: 'Modernizr' },
+        'd3': { exports: 'd3' },
+        'three': { exports: 'THREE' }
+    },
+    paths: {
+        'app': '../app'
+    }
 });
